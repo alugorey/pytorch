c3db40ef554f1730d2ea9fdeabfa9f320fbd14e2
Andres Lugo-Reyes <Andy.LugoReyes@amd.com>
2022-08-23 14:24:30 -0700

    Force driver to gesvdj

--- aten/src/ATen/native/cuda/linalg/BatchLinearAlgebraLib.cpp
+++ aten/src/ATen/native/cuda/linalg/BatchLinearAlgebraLib.cpp
@@ -951,7 +951,7 @@ void svd_cusolver(const Tensor& A,
   static const char* check_svd_doc = "Check doc at https://pytorch.org/docs/stable/generated/torch.linalg.svd.html";
 
   // The default heuristic is to use gesvdj driver
-  const auto driver_v = driver.value_or("gesvdj");
+  const auto driver_v = c10::string_view("gesvdj");
 
   if (driver_v == "gesvd") {
     svd_cusolver_gesvd(A, U, S, V, info, full_matrices, compute_uv);
