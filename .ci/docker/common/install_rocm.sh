ff9f673f06048cd443a2b58e46a5284fdc466588
Pruthvi Madugundu <pruthvigithub@gmail.com>
2024-12-21 11:16:42 -0600

    Remove miopen-hip-gfx package installation

--- .ci/docker/common/install_rocm.sh
+++ .ci/docker/common/install_rocm.sh
@@ -45,12 +45,13 @@ install_ubuntu() {
     # precompiled miopen kernels added in ROCm 3.5, renamed in ROCm 5.5
     # search for all unversioned packages
     # if search fails it will abort this script; use true to avoid case where search fails
-    MIOPENHIPGFX=$(apt-cache search --names-only miopen-hip-gfx | awk '{print $1}' | grep -F -v . || true)
-    if [[ "x${MIOPENHIPGFX}" = x ]]; then
-      echo "miopen-hip-gfx package not available" && exit 1
-    else
-      DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated ${MIOPENHIPGFX}
-    fi
+    # MI355 is not having MIOpen-hip-gfx packages
+    #MIOPENHIPGFX=$(apt-cache search --names-only miopen-hip-gfx | awk '{print $1}' | grep -F -v . || true)
+    #if [[ "x${MIOPENHIPGFX}" = x ]]; then
+    #  echo "miopen-hip-gfx package not available" && exit 1
+    #else
+    #  DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated ${MIOPENHIPGFX}
+    #fi
 
     # ROCm 6.0 had a regression where journal_mode was enabled on the kdb files resulting in permission errors at runtime
     for kdb in /opt/rocm/share/miopen/db/*.kdb
